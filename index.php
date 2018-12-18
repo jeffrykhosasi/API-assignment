8eee7803f6bc1b5dada08a92b6832b98e5fe8d84
jeffrykhosasi <jeffrywidkaja@gmail.com>
2018-12-18 11:17:02 +0700

    berhasil la1

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    require_once "config.php"; //membutuhkan oauth google yang berisi client id , client secret id dan uri yang berada pada google
+    require_once "auth/config.php"; //membutuhkan oauth google yang berisi client id , client secret id dan uri yang berada pada google
 
 	if (isset($_SESSION['access_token'])) { // jika access token tersedia
 		header('Location: auth/check.php'); //pindah ke index.php
