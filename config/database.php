aace67be685e16de5af69b4d5421fc95c1bba390
jeffrykhosasi <jeffrywidkaja@gmail.com>
2018-12-19 00:34:04 +0700

    bisa

--- config/database.php
+++ config/database.php
@@ -1,28 +1,53 @@
 <?php
 	class Database
 	{
-		//credential database
-		private $host = "localhost";
-		private $db_name = "api_db";
-		private $username = "root";
-		private $password = "";
-		public $conn;
+		// //credential database
+		// private $host = "localhost";
+		// private $db_name = "api_db";
+		// private $username = "root";
+		// private $password = "";
+		// public $conn;
 		
-		//connect to database
+		// //connect to database
+		// public function getConnection()
+		// {
+		// 	$this->conn = null;
+			
+		// 	try
+		// 	{
+		// 		$this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+		// 		$this->conn->exec("set names utf8");
+		// 	}
+		// 	catch(PDOException $exception)
+		// 	{
+		// 		echo "Connection error: " . $exception->getMessage();
+		// 	}
+		// 	return $this->conn;
+		// }	
+		
+
+		$host = "ec2-23-23-235-86.compute-1.amazonaws.com";
+		$user = "vytogaqpgryrfs";
+		$password = "fa64d7f533ccda8b6f7d55e14412d90b76d29b0cc123b1ceba2ca26aa1511fea";
+		$dbname = "dcps966p2ecrlu";
+		$port = "5432";
+		public $conn;
 		public function getConnection()
 		{
 			$this->conn = null;
-			
-			try
-			{
-				$this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-				$this->conn->exec("set names utf8");
-			}
-			catch(PDOException $exception)
-			{
-				echo "Connection error: " . $exception->getMessage();
-			}
+		try{
+			 //Set DSN data source name
+			 $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
+			 //create a pdo instance
+			 $pdo = new PDO($dsn, $user, $password);
+			 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+			 $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+			 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		   }
+		   catch (PDOException $e) {
+		   echo 'Connection failed: ' . $e->getMessage();
+		   }
 			return $this->conn;
-		}	
+		}
 	}
 ?>
